962d94b058657542857cccd5d8335df4eed7077a
gitcuaquan <79095757+gitcuaquan@users.noreply.github.com>
2023-12-01 17:49:29 +0700

    first commit

---
+++ xeno/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+const greeting = ref("")
+
+const update_greeting = (_greeting) => {
+  greeting.value = _greeting
+}
+const click = () => {
+  eel.get_greeting("user")(update_greeting)
+  eel.hello_world()
+  console.log("clicked")
+}
+</script>
+
+<template>
+  <div class="flex">
+    <p>{{ greeting }}</p>
+    <button type="button" class="p-2 border border-red-500 rounded-md" @click="click()">Click me</button>
+  </div>
+</template>
